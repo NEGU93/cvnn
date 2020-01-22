Multiclass classification fixed. Bug on normalization

src/cvnn_v1_compat.py
@@ -72,7 +72,7 @@ class Cvnn:
 
         # logs dir
         self.now = datetime.today().strftime("%Y%m%d%H%M%S")
-        project_path = os.path.abspath("./")
+        project_path = os.path.abspath("../")
         root_dir = project_path + "/log/{}/run-{}/".format(self.name, self.now)
         # Tensorboard
         self.tbdir = root_dir + "tensorboard_logs/"
@@ -185,19 +185,22 @@ class Cvnn:
 
             # Run validation at beginning
             self.print_validation_loss(0, x_test, y_test)
+
+            # Run train
             num_tr_iter = int(len(y_train) / batch_size)  # Number of training iterations in each epoch
             for epoch in range(epochs):
                 # Randomly shuffle the training data at the beginning of each epoch
                 x_train, y_train = randomize(x_train, y_train)
                 for iteration in range(num_tr_iter):
+                    # Get the batch
                     start = iteration * batch_size
                     end = (iteration + 1) * batch_size
                     x_batch, y_batch = get_next_batch(x_train, y_train, start, end)
                     # Run optimization op (backpropagation)
                     feed_dict_batch = {self.X: x_batch, self.y: y_batch}
-                    self.sess.run(self.training_op, feed_dict=feed_dict_batch)
                     if (epoch * batch_size + iteration) % display_freq == 0:
                         self.run_checkpoint(epoch, num_tr_iter, iteration, feed_dict_batch)
+                    self.sess.run(self.training_op, feed_dict=feed_dict_batch)
 
             # Run validation at the end
             feed_dict_valid = {self.X: x_test, self.y: y_test}
@@ -241,25 +244,28 @@ class Cvnn:
     # Layers
     def _create_dense_layer(self, input_size, output_size, input, layer_number):
         with tf.compat.v1.name_scope("dense_layer_" + str(layer_number)) as scope:
-            w = tf.Variable(self.glorot_uniform_init(input_size, output_size).astype(np.float32),
+            w = tf.Variable(tf.keras.initializers.GlorotUniform()(shape=(input_size, output_size)),
                             name="weights" + str(layer_number))
-            b = tf.Variable(np.zeros(output_size).astype(np.float32), name="bias" + str(layer_number))
+            b = tf.Variable(tf.zeros(output_size), name="bias" + str(layer_number))
             if self.tensorboard:
                 tf.compat.v1.summary.histogram('real_weight_' + str(layer_number), w)
+                tf.compat.v1.summary.histogram('real_bias_' + str(layer_number), b)
             return tf.add(tf.matmul(input, w), b), [w, b]
 
     def _create_complex_dense_layer(self, input_size, output_size, input_of_layer, layer_number):
         # TODO: treat bias as a weight. It might optimize training (no add operation, only mult)
         with tf.compat.v1.name_scope("dense_layer_" + str(layer_number)) as scope:
             # Create weight matrix initialized randomely from N~(0, 0.01)
-            w = tf.Variable(tf.complex(self.glorot_uniform_init(input_size, output_size).astype(np.float32),
-                                       self.glorot_uniform_init(input_size, output_size).astype(np.float32)),
+            w = tf.Variable(tf.complex(tf.keras.initializers.GlorotUniform()(shape=(input_size, output_size)),
+                                       tf.keras.initializers.GlorotUniform()(shape=(input_size, output_size))),
                             name="weights" + str(layer_number))
-            b = tf.Variable(tf.complex(np.zeros(output_size).astype(np.float32),
-                                       np.zeros(output_size).astype(np.float32)), name="bias" + str(layer_number))
+            b = tf.Variable(tf.complex(tf.zeros(output_size),
+                                       tf.zeros(output_size)), name="bias" + str(layer_number))
             if self.tensorboard:
                 tf.compat.v1.summary.histogram('real_weight_' + str(layer_number), tf.math.real(w))
                 tf.compat.v1.summary.histogram('imag_weight_' + str(layer_number), tf.math.imag(w))
+                tf.compat.v1.summary.histogram('real_bias_' + str(layer_number), tf.math.real(b))
+                tf.compat.v1.summary.histogram('imag_bias_' + str(layer_number), tf.math.imag(b))
             return tf.add(tf.matmul(input_of_layer, w), b), [w, b]
 
     def _create_graph_from_shape(self, shape, input_dtype=np.complex64, output_dtype=np.float32):
@@ -530,9 +536,9 @@ class Cvnn:
     def _apply_loss(self, loss_func):
         # TODO: don't like the fact that I have to give self to this and not to apply_activation
         if callable(loss_func):
-            if loss_func.__module__ != 'losses':
+            if loss_func.__module__ != 'losses' and loss_func.__module__ != 'tensorflow.python.keras.losses':
                 sys.exit("Cvnn::_apply_loss: Unknown loss function.\n\t "
-                         "Can only use losses declared on losses.py")
+                         "Can only use losses declared on losses.py or tensorflow.python.keras.losses")
         elif isinstance(loss_func, str):
             try:
                 loss_func = loss_dispatcher[loss_func]
@@ -541,7 +547,7 @@ class Cvnn:
         else:
             sys.exit("Cvnn::_apply_loss: Invalid loss function")
 
-        return loss_func(self.y_out, self.y)
+        return tf.reduce_mean(input_tensor=loss_func(self.y, self.y_out), name=loss_func.__name__)
 
     """-----------
     # Initializers
@@ -609,7 +615,7 @@ __author__ = 'J. Agustin BARRACHINA'
 __copyright__ = 'Copyright 2020, {project_name}'
 __credits__ = ['{credit_list}']
 __license__ = '{license}'
-__version__ = '1.0.1'
+__version__ = '1.0.2'
 __maintainer__ = 'J. Agustin BARRACHINA'
 __email__ = 'joseagustin.barra@gmail.com; jose-agustin.barrachina@centralesupelec.fr'
 __status__ = '{dev_status}'

src/data_processing.py
@@ -76,7 +76,6 @@ noise_gen_dispatcher = {
 
 def _create_gaussian_noise(m, n, num_classes=2, noise_type='hilbert'):
     """
-
     :param m: Number of examples per class
     :param n: Size of vector
     :param num_classes: Number of different classes to be made
@@ -89,7 +88,7 @@ def _create_gaussian_noise(m, n, num_classes=2, noise_type='hilbert'):
     y = np.zeros((num_classes*m, num_classes))      # Initialize all at 0 to later put a 1 on the corresponding place
     for k in range(num_classes):
         mu = int(100*np.random.rand())
-        sigma = 0*np.random.rand()
+        sigma = 30*np.random.rand()
         print("Class " + str(k) + ": mu = " + str(mu) + "; sigma = " + str(sigma))
         try:
             x[k*m:(k+1)*m, :] = noise_gen_dispatcher[noise_type](m, n, mu, sigma)
@@ -97,6 +96,22 @@ def _create_gaussian_noise(m, n, num_classes=2, noise_type='hilbert'):
             sys.exit("_create_gaussian_noise: Unknown type of noise")
         y[k*m:(k+1)*m, k] = 1
 
+    return normalize(x), y
+
+
+def _create_constant_classes(m, n, num_classes=2, vect=None):
+    assert len(vect) == num_classes     # TODO: message of error
+    x = np.empty((num_classes * m, n)) + 1j * np.empty((num_classes * m, n))
+    y = np.zeros((num_classes * m, num_classes))  # Initialize all at 0 to later put a 1 on the corresponding place
+    for k in range(num_classes):
+        if vect is None:
+            const = np.random.randint(100)
+        else:
+            const = vect[k]
+        x[k * m:(k + 1) * m, :] = np.ones((m, n))*const + 1j * np.ones((m, n))*const
+        y[k * m:(k + 1) * m, k] = 1
+        print("Class " + str(k) + " value = " + str(const))
+    # set_trace()
     return normalize(x), y
 
 
@@ -108,6 +123,12 @@ def get_non_correlated_gaussian_noise(m, n, num_classes=2):
 
 def get_gaussian_noise(m, n, num_classes=2, noise_type='hilbert'):
     x, y = _create_gaussian_noise(m, n, num_classes, noise_type)
+    x, y = randomize(x, y)
+    return separate_into_train_and_test(x, y)
+
+
+def get_constant(m, n, num_classes=2, vect=None):
+    x, y = _create_constant_classes(m, n, num_classes, vect)
     x, y = randomize(x, y)
     return separate_into_train_and_test(x, y)
 
@@ -215,6 +236,6 @@ if __name__ == "__main__":
 
 
 __author__ = 'J. Agustin BARRACHINA'
-__version__ = '1.0.1'
+__version__ = '1.0.2'
 __maintainer__ = 'J. Agustin BARRACHINA'
 __email__ = 'joseagustin.barra@gmail.com; jose-agustin.barrachina@centralesupelec.fr'

src/losses.py
@@ -13,14 +13,14 @@ Loss functions will be type agnostic, meaning it can be with either complex or r
 ---------"""
 
 
-def mean_square(y_out, y):
+def mean_square(y, y_out):
     """
     Mean Squared Error, or L2 loss.
     :return:
     """
     with tf.compat.v1.name_scope("loss") as scope:
         error = y - y_out
-        return tf.reduce_mean(input_tensor=tf.square(tf.abs(error)), name="loss")
+        return tf.square(tf.abs(error))
 
 
 """-------------
@@ -28,9 +28,10 @@ def mean_square(y_out, y):
 -------------"""
 
 
-def categorical_crossentropy(y_out, y):
+def categorical_crossentropy(y, y_out):
     """
     https://jovianlin.io/cat-crossentropy-vs-sparse-cat-crossentropy/
+    https://stats.stackexchange.com/questions/260505/should-i-use-a-categorical-cross-entropy-or-binary-cross-entropy-loss-for-binary
     :return: -y*log(y_out)-(1-y)*log(1-y_out) where:
         log - the natural log
         y - binary indicator (0 or 1), it will be all 0's but one (according to the corresponding class)
@@ -39,15 +40,15 @@ def categorical_crossentropy(y_out, y):
     with tf.compat.v1.name_scope("loss_scope") as scope:
         y1_error = tf.math.multiply(-y, tf.math.log(y_out))  # Error for y = 1
         y0_error = tf.math.multiply(1 - y, tf.math.log(1 - y_out))  # Error for y = 0
-        error = tf.math.subtract(y1_error, y0_error)
-        return tf.reduce_mean(input_tensor=error, name="loss")
+        error = tf.keras.backend.sum(tf.math.subtract(y1_error, y0_error), 1)   # Sum of every
+        return error
 
 
 __author__ = 'J. Agustin BARRACHINA'
 __copyright__ = 'Copyright 2020, {project_name}'
 __credits__ = ['{credit_list}']
 __license__ = '{license}'
-__version__ = '1.0.1'
+__version__ = '1.0.2'
 __maintainer__ = 'J. Agustin BARRACHINA'
 __email__ = 'joseagustin.barra@gmail.com; jose-agustin.barrachina@centralesupelec.fr'
 __status__ = '{dev_status}'

src/main.py
@@ -8,8 +8,8 @@ from pdb import set_trace
 import os
 
 
-def monte_carlo_loss_gaussian_noise(iterations=1000, m=100000, n=1000, num_classes=2, path="./results/",
-                                    filename="histogram.csv"):
+def monte_carlo_cvnn_rvnn_compare(iterations=1000, m=100000, n=100, num_classes=3, path="../results/",
+                                    filename="histogram.csv", name=''):
     """
     Computes the CVNN and RVNN loss result on classification of random gaussian noise over the amount of
     iterations gives as a parameter. Saves results appended to a new csv.
@@ -24,17 +24,23 @@ def monte_carlo_loss_gaussian_noise(iterations=1000, m=100000, n=1000, num_class
 
     file = open(path + filename, 'a')  # TODO: create directory if it does not exist
     if write:
-        file.write("CVNN loss, RVNN loss\n")
+        file.write("CVNN loss, CVNN acc, RVNN loss, RVNN acc\n")
     for i in range(iterations):
-        x_train, y_train, x_test, y_test = dp.get_non_correlated_gaussian_noise(m, n, num_classes)
-        x_train_real, x_test_real = dp.get_real_train_and_test(x_train, x_test)
-        cvnn, rvnn = do_one_iter(x_train, y_train, x_train_real, x_test, y_test, x_test_real)
+        for k in range(num_classes):
+            x_train, y_train, x_test, y_test = dp.get_gaussian_noise(m, n, k+2, name)
+            # TODO: Better error message if I don't do the k +1
+            x_train_real, x_test_real = dp.get_real_train_and_test(x_train, x_test)
+            cvnn, rvnn = do_one_iter(x_train, y_train, x_train_real, x_test, y_test, x_test_real)
 
-        # compare them
-        file.write(str(cvnn.compute_loss(x_test, y_test)))
-        file.write(", ")
-        file.write(str(rvnn.compute_loss(x_test_real, y_test)))
-        file.write("\n")
+            # compare them
+            file.write(str(cvnn.compute_loss(x_test, y_test)))
+            file.write(", ")
+            file.write(str(cvnn.compute_accuracy(x_test, y_test)))
+            file.write(", ")
+            file.write(str(rvnn.compute_loss(x_test_real, y_test)))
+            file.write(", ")
+            file.write(str(rvnn.compute_accuracy(x_test_real, y_test)))
+            file.write("\n")
 
     file.close()
 
@@ -52,7 +58,7 @@ def do_one_iter(x_train, y_train, x_train_real, x_test, y_test, x_test_real, nam
     shape_rvnn = [(2 * input_size, 'ignored'),
                   (2 * hidden_size, tf.keras.activations.sigmoid),
                   (output_size, tf.keras.activations.softmax)]
-    name = "_1HL_for_" + name + "gauss_noise"
+    name = "_1HL_for_" + name + "_noise"
 
     auto_restore = False
     cvnn = Cvnn("CVNN" + name, automatic_restore=auto_restore)
@@ -60,8 +66,8 @@ def do_one_iter(x_train, y_train, x_train_real, x_test, y_test, x_test_real, nam
 
     if not auto_restore:
         # cvnn.create_linear_regression_graph(input_size, output_size)
-        cvnn.create_mlp_graph("categorical_crossentropy", shape_cvnn, input_dtype=np.complex64)
-        rvnn.create_mlp_graph("categorical_crossentropy", shape_rvnn, input_dtype=np.float32)
+        cvnn.create_mlp_graph(tf.keras.losses.categorical_crossentropy, shape_cvnn, input_dtype=np.complex64)
+        rvnn.create_mlp_graph(tf.keras.losses.categorical_crossentropy, shape_rvnn, input_dtype=np.float32)
 
     cvnn.train(x_train, y_train, x_test, y_test, epochs=10)
     rvnn.train(x_train_real, y_train, x_test_real, y_test, epochs=10)
@@ -75,19 +81,34 @@ if __name__ == "__main__":
     # monte_carlo_loss_gaussian_noise(iterations=100, filename="historgram_gaussian.csv")
     m = 100000
     n = 100
-    num_classes = 4
+    num_classes = 5
     name = 'hilbert'
+    # random
+    # vect = None
+    # work
+    # vect = [42, 51, 37]
+    # vect = [42, 51, 37]
+    # vect = [42, 51, 37]
+    # vect = [42, 51, 37]
+    # Doesn't work
+    # vect = [42, 51, 37]
+
+    monte_carlo_cvnn_rvnn_compare(iterations=100, filename="historgram_gaussian.csv", name=name)
+    """
     x_train, y_train, x_test, y_test = dp.get_gaussian_noise(m, n, num_classes, name)
+    # x_train, y_train, x_test, y_test = dp.get_constant(m, n, num_classes, vect)
     x_train_real, x_test_real = dp.get_real_train_and_test(x_train, x_test)
 
     cvnn, rvnn = do_one_iter(x_train, y_train, x_train_real, x_test, y_test, x_test_real, name)
 
     # TODO: There is a bug in here. It does not work with multiple classes. Loss function probably.
     print(da.categorical_confusion_matrix(cvnn.predict(x_test), y_test))
+    print(da.categorical_confusion_matrix(rvnn.predict(x_test_real), y_test))
 
     set_trace()
+    """
 
 __author__ = 'J. Agustin BARRACHINA'
-__version__ = '1.0.2'
+__version__ = '1.0.3'
 __maintainer__ = 'J. Agustin BARRACHINA'
 __email__ = 'joseagustin.barra@gmail.com; jose-agustin.barrachina@centralesupelec.fr'

src/utils.py
@@ -57,7 +57,7 @@ def get_next_batch(x, y, start, end):
 
 
 def normalize(x):
-    return (x-np.amin(x))/(np.amax(x)-np.amin(x))     # Checked it works for complex values
+    return (x-np.amin(x))/np.abs(np.amax(x)-np.amin(x))     # Checked it works for complex values
 
 
 def tensorflow_argmax_np_equivalent(x, num_classes):
@@ -74,6 +74,6 @@ def compute_accuracy(x, y):
 
 
 __author__ = 'J. Agustin BARRACHINA'
-__version__ = '1.0.1'
+__version__ = '1.0.2'
 __maintainer__ = 'J. Agustin BARRACHINA'
 __email__ = 'joseagustin.barra@gmail.com; jose-agustin.barrachina@centralesupelec.fr'