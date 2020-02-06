afcd6a22f64dc172c619b84d7a84e548d319ca71
2019barrachija <jose-agustin.barrachina@centralesupelec.fr>
2020-02-06 14:54:11 +0100

    stupid change to chech auto-version-commit-script

--- cvnn/cvnn_v1_compat.py
+++ cvnn/cvnn_v1_compat.py
@@ -620,7 +620,6 @@ if __name__ == "__main__":
 
     print(da.categorical_confusion_matrix(cvnn.predict(x_test), y_test, "output.png"))
     cvnn.plot_loss()
-    set_trace()
 
 # How to comment script header
 # https://medium.com/@rukavina.andrei/how-to-write-a-python-script-header-51d3cec13731
@@ -628,7 +627,7 @@ __author__ = 'J. Agustin BARRACHINA'
 __copyright__ = 'Copyright 2020, {project_name}'
 __credits__ = ['{credit_list}']
 __license__ = '{license}'
-__version__ = '0.1.5'
+__version__ = '0.1.6'
 __maintainer__ = 'J. Agustin BARRACHINA'
 __email__ = 'joseagustin.barra@gmail.com; jose-agustin.barrachina@centralesupelec.fr'
 __status__ = '{dev_status}'
