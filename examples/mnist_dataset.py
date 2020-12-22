0d2ec95b66d04713262087a1383d7432dfed6379
NEGU93 <joseagustin.barra@gmail.com>
2020-12-22 10:47:40 +0100

    Added fashon mnist example, gives exact same result

--- examples/mnist_dataset.py
+++ examples/mnist_dataset.py
@@ -12,7 +12,8 @@ try:
 except ModuleNotFoundError:
     PLOTLY = False
 
-tfds.disable_progress_bar()
+# tf.enable_v2_behavior()
+# tfds.disable_progress_bar()
 
 PLOTLY_CONFIG = {
     'scrollZoom': True,
@@ -29,14 +30,14 @@ def get_dataset():
     (ds_train, ds_test), ds_info = tfds.load(
         'mnist',
         split=['train', 'test'],
-        shuffle_files=True,
+        shuffle_files=False,
         as_supervised=True,
         with_info=True,
     )
 
     ds_train = ds_train.map(normalize_img, num_parallel_calls=tf.data.experimental.AUTOTUNE)
     ds_train = ds_train.cache()
-    ds_train = ds_train.shuffle(ds_info.splits['train'].num_examples)
+    # ds_train = ds_train.shuffle(ds_info.splits['train'].num_examples)
     ds_train = ds_train.batch(128)
     ds_train = ds_train.prefetch(tf.data.experimental.AUTOTUNE)
 
@@ -48,12 +49,12 @@ def get_dataset():
     return ds_train, ds_test
 
 
-def keras_fit(ds_train, ds_test, verbose=True):
+def keras_fit(ds_train, ds_test, verbose=True, init1='glorot_uniform', init2='glorot_uniform'):
     # https://www.tensorflow.org/datasets/keras_example
     model = tf.keras.models.Sequential([
       tf.keras.layers.Flatten(input_shape=(28, 28, 1)),
-      tf.keras.layers.Dense(128, activation='relu'),
-      tf.keras.layers.Dense(10, activation='softmax')
+      tf.keras.layers.Dense(128, activation='relu', kernel_initializer=init1),
+      tf.keras.layers.Dense(10, activation='softmax', kernel_initializer=init2)
     ])
     model.compile(
         loss='sparse_categorical_crossentropy',
@@ -65,17 +66,17 @@ def keras_fit(ds_train, ds_test, verbose=True):
         ds_train,
         epochs=6,
         validation_data=ds_test,
-        verbose=verbose
+        verbose=verbose, shuffle=False
     )
     stop = timeit.default_timer()
     return model.evaluate(ds_test, verbose=verbose), stop - start
 
 
-def own_fit(ds_train, ds_test, verbose=True):
+def own_fit(ds_train, ds_test, verbose=True, init1='glorot_uniform', init2='glorot_uniform'):
     model = tf.keras.models.Sequential([
         layers.ComplexFlatten(input_shape=(28, 28, 1), dtype=np.float32),
-        layers.ComplexDense(128, activation='cart_relu', dtype=np.float32),
-        layers.ComplexDense(10, activation='softmax_real', dtype=np.float32)
+        layers.ComplexDense(128, activation='cart_relu', dtype=np.float32, kernel_initializer=init1),
+        layers.ComplexDense(10, activation='softmax_real', dtype=np.float32, kernel_initializer=init2)
     ])
     model.compile(
         loss='sparse_categorical_crossentropy',
@@ -87,7 +88,7 @@ def own_fit(ds_train, ds_test, verbose=True):
         ds_train,
         epochs=6,
         validation_data=ds_test,
-        verbose=verbose
+        verbose=verbose, shuffle=False
     )
     stop = timeit.default_timer()
     return model.evaluate(ds_test, verbose=verbose), stop - start
@@ -155,8 +156,26 @@ def test_mnist_montecarlo():
     f.close()
 
 
+def test_mnist():
+    seed = 117
+    init = tf.keras.initializers.GlorotUniform(seed=seed)
+    init1 = tf.constant_initializer(init((784, 128)).numpy())
+    init2 = tf.constant_initializer(init((128, 10)).numpy())
+    ds_train, ds_test = get_dataset()
+    keras1 = keras_fit(ds_train, ds_test, init1=init1, init2=init2)
+    keras2 = keras_fit(ds_train, ds_test, init1=init1, init2=init2)
+    own = own_fit(ds_train, ds_test, init1=init1, init2=init2)
+    print(keras1)
+    print(keras2)
+    print(own)
+    # set_trace()
+    
+def test_fashion_mnist():
+    
+
 if __name__ == "__main__":
-    test_mnist_montecarlo()
+    test_mnist()
+    # test_mnist_montecarlo()
     # ds_train, ds_test = get_dataset()
     # keras_fit(ds_train, ds_test)
     # own_fit(ds_train, ds_test)
