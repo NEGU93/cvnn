eb3ea2eb9227b5c5e2bb3bbdbddfa056a3fe168e
NEGU93 <joseagustin.barra@gmail.com>
2021-03-01 17:53:46 +0100

    debugging results differences

--- examples/random_noise_publication.py
+++ examples/random_noise_publication.py
@@ -1,6 +1,6 @@
 from cvnn.montecarlo import run_gaussian_dataset_montecarlo
 
-run_gaussian_dataset_montecarlo(iterations=1000, m=10000, n=128, param_list=None,
-                                epochs=150, batch_size=100, display_freq=1, optimizer='Adam',
+run_gaussian_dataset_montecarlo(iterations=500, m=10000, n=128, param_list=None,
+                                epochs=150, batch_size=100, optimizer='sgd',
                                 shape_raw=None, activation='cart_relu', debug=False,
                                 polar=False, do_all=True, dropout=None)
