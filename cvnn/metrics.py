fixed averageaccuracy for when labels are lacking

cvnn/metrics.py
@@ -115,20 +115,23 @@ def _accuracy(y_true, y_pred):
 
 
 def custom_average_accuracy(y_true, y_pred):
+    # Mask to remove the labels (y_true) that are zero: ex. [0, 0, 0]
     remove_zeros_mask = tf.math.logical_not(tf.math.reduce_all(tf.math.logical_not(tf.cast(y_true, bool)), axis=-1))
     y_true = tf.boolean_mask(y_true, remove_zeros_mask)
     y_pred = tf.boolean_mask(y_pred, remove_zeros_mask)
     num_cls = y_true.shape[-1]
-    y_pred = tf.math.argmax(y_pred, axis=-1)
+    y_pred = tf.math.argmax(y_pred, axis=-1)        # ex. [0, 0, 1] -> [2]
     y_true = tf.math.argmax(y_true, axis=-1)
-    accuracies = []
+    accuracies = tf.TensorArray(tf.float32, size=0, dynamic_size=True)
     for i in range(0, num_cls):
         cls_mask = y_true == i
-        # set_trace()
-        accuracies.append(_accuracy(y_true=tf.boolean_mask(y_true, cls_mask),
-                                    y_pred=tf.boolean_mask(y_pred, cls_mask)))
-    accuracies = tf.convert_to_tensor(accuracies)
-    return tf.math.reduce_sum(accuracies) / len(accuracies)
+        cls_y_true = tf.boolean_mask(y_true, cls_mask)
+        if not tf.equal(tf.size(cls_y_true), 0):
+            new_acc = _accuracy(y_true=cls_y_true, y_pred=tf.boolean_mask(y_pred, cls_mask))
+            accuracies = accuracies.write(accuracies.size(), new_acc)
+    # import pdb; pdb.set_trace()
+    accuracies = accuracies.stack()
+    return tf.math.reduce_sum(accuracies) / tf.cast(len(accuracies), dtype=accuracies.dtype)
 
 
 class ComplexAverageAccuracy(Mean):
@@ -144,7 +147,8 @@ class ComplexAverageAccuracy(Mean):
         if y_pred.dtype.is_complex:
             y_pred = (tf.math.real(y_pred) + tf.math.imag(y_pred)) / 2
         if y_true.dtype.is_complex:
-            assert tf.math.reduce_all(tf.math.real(y_pred) == tf.math.imag(y_pred)), "y_pred must be real valued"
+            assert tf.math.reduce_all(tf.math.real(y_true) == tf.math.imag(y_true)), "y_pred must be real valued"
+            y_true = tf.math.real(y_true)
         matches = self._fn(y_true, y_pred)
         return super(ComplexAverageAccuracy, self).update_state(matches)
 

tests/test_metrics.py
@@ -1,6 +1,78 @@
 import numpy as np
+from tensorflow.keras.metrics import CategoricalAccuracy
+import tensorflow as tf
 from pdb import set_trace
 from cvnn.metrics import ComplexAverageAccuracy, ComplexCategoricalAccuracy
+
+
+def test_with_tf():
+    classes = 3
+    y_true = tf.cast(tf.random.uniform(shape=(34, 54, 12), maxval=classes), dtype=tf.int32)
+    y_pred = tf.cast(tf.random.uniform(shape=y_true.shape, maxval=classes), dtype=tf.int32)
+    y_pred_one_hot = tf.one_hot(y_pred, depth=classes)
+    y_true_one_hot = tf.one_hot(y_true, depth=classes)
+    tf_metric = CategoricalAccuracy()
+    tf_metric.update_state(y_true_one_hot, y_pred_one_hot)
+    own_metric = ComplexCategoricalAccuracy()
+    own_metric.update_state(y_true_one_hot, y_pred_one_hot)
+    # set_trace()
+    assert own_metric.result().numpy() == tf_metric.result().numpy()
+    y_true = np.array([
+        [1., 0., 0., 0.],  # 1
+        [1., 0., 0., 0.],  # 1
+        [1., 0., 0., 0.],  # 1
+        [1., 0., 0., 0.],  # 1
+        [0., 0., 0., 0.],  # This shows tf does not ignore cases with [0. 0. 0. 0.] (unlabeled)
+        [0., 0., 1., 0.],  # 3
+        [0., 0., 1., 0.],  # 3
+        [0., 0., 0., 0.],  # 3
+        [0., 0., 1., 0.]  # 3
+    ])
+    y_pred = np.array([
+        [1., 0., 0., 0.],  # 1
+        [1., 0., 0., 0.],  # 1
+        [1., 0., 0., 0.],  # 1
+        [1., 0., 0., 0.],  # 1
+        [1., 0., 0., 0.],  # 1
+        [0., 0., 0., 1.],  # 4
+        [0., 0., 0., 1.],  # 4
+        [0., 0., 0., 1.],  # 4
+        [0., 0., 0., 1.]  # 4
+    ])
+    tf_metric = CategoricalAccuracy()
+    tf_metric.update_state(y_true, y_pred)
+    own_metric = ComplexCategoricalAccuracy()
+    own_metric.update_state(y_true, y_pred, ignore_unlabeled=False)     # to make it as tf
+    assert own_metric.result().numpy() == tf_metric.result().numpy()
+    y_true = np.array([
+        [1., 0.],
+        [1., 0.],
+        [1., 0.],
+        [1., 0.],
+        [1., 0.],
+        [1., 0.],
+        [1., 0.],
+        [1., 0.],
+        [1., 0.],
+        [0., 1.]
+    ])
+    y_pred = np.array([
+        [1., 0.],
+        [1., 0.],
+        [1., 0.],
+        [1., 0.],
+        [1., 0.],
+        [1., 0.],
+        [1., 0.],
+        [1., 0.],
+        [1., 0.],
+        [1., 0.]
+    ])
+    tf_metric = CategoricalAccuracy()
+    tf_metric.update_state(y_true, y_pred)
+    own_metric = ComplexCategoricalAccuracy()
+    own_metric.update_state(y_true, y_pred, ignore_unlabeled=False)  # to make it as tf
+    assert own_metric.result().numpy() == tf_metric.result().numpy()
 
 
 def test_metric():
@@ -19,7 +91,6 @@ def test_metric():
     m = ComplexAverageAccuracy()
     m.update_state(y_true, y_pred)
     assert m.result().numpy() == np.cast['float32'](1/6)     # I want 0.5/3 = 1/6
-
     y_true = np.array([
         [1., 0.],
         [1., 0.],
@@ -52,5 +123,103 @@ def test_metric():
     assert m.result().numpy() == np.cast['float32'](0.5)
 
 
+def test_null_label():
+    y_true = np.array([
+        [1., 0., 0., 0.],  # 1
+        [1., 0., 0., 0.],  # 1
+        [1., 0., 0., 0.],  # 1
+        [1., 0., 0., 0.],  # 1
+        [1., 0., 0., 0.],  # 1
+        [0., 0., 1., 0.],  # 3
+        [0., 0., 1., 0.],  # 3
+        [0., 0., 1., 0.],  # 3
+        [0., 0., 1., 0.]  # 3
+    ])
+    y_pred = np.array([
+        [1., 0., 0., 0.],  # 1
+        [1., 0., 0., 0.],  # 1
+        [1., 0., 0., 0.],  # 1
+        [1., 0., 0., 0.],  # 1
+        [1., 0., 0., 0.],  # 1
+        [0., 0., 0., 1.],  # 4
+        [0., 0., 0., 1.],  # 4
+        [0., 0., 0., 1.],  # 4
+        [0., 0., 0., 1.]  # 4
+    ])
+    m = ComplexAverageAccuracy()
+    m.update_state(y_true, y_pred)
+    assert m.result().numpy() == np.cast['float32'](0.5)
+    y_true = np.array([
+        [1., 0.],
+        [1., 0.],
+        [1., 0.],
+        [1., 0.],
+        [1., 0.],
+        [1., 0.],
+        [1., 0.],
+        [1., 0.],
+        [1., 0.],
+        [0., 0.],
+        [0., 0.],
+        [0., 0.],
+        [0., 0.],
+        [0., 1.]
+    ])
+    y_pred = np.array([
+        [1., 0.],
+        [1., 0.],
+        [1., 0.],
+        [1., 0.],
+        [1., 0.],
+        [1., 0.],
+        [1., 0.],
+        [1., 0.],
+        [1., 0.],
+        [0., 1.],
+        [0., 1.],
+        [0., 1.],
+        [0., 1.],
+        [1., 0.]
+    ])
+    m = ComplexCategoricalAccuracy()
+    m.update_state(y_true, y_pred)
+    # tf_metric = CategoricalAccuracy()
+    # tf_metric.update_state(y_true, y_pred)
+    assert m.result().numpy() == np.cast['float32'](.9)
+    # assert m.result().numpy() == tf_metric.result().numpy()
+    m = ComplexAverageAccuracy()
+    m.update_state(y_true, y_pred)
+    assert m.result().numpy() == np.cast['float32'](0.5)
+    y_true = np.array([
+        [1., 0., 0., 0.],   # 1
+        [1., 0., 0., 0.],   # 1
+        [1., 0., 0., 0.],   # 1
+        [1., 0., 0., 0.],   # 1
+        [1., 0., 0., 0.],   # 1
+        [0., 0., 1., 0.],   # 3
+        [0., 0., 1., 0.],   # 3
+        [0., 0., 1., 0.],   # 3
+        [0., 0., 0., 1.],   # 4
+        [0., 1., 0., 0.]    # 2
+    ])
+    y_pred = np.array([
+        [1., 0., 0., 0.],   # 1
+        [1., 0., 0., 0.],   # 1
+        [1., 0., 0., 0.],   # 1
+        [1., 0., 0., 0.],   # 1
+        [1., 0., 0., 0.],   # 1
+        [0., 0., 0., 1.],   # 4
+        [0., 0., 0., 1.],   # 4
+        [0., 0., 0., 1.],   # 4
+        [0., 0., 0., 1.],   # 4
+        [0., 1., 0., 0.]    # 2
+    ])
+    m = ComplexAverageAccuracy()
+    m.update_state(y_true, y_pred)
+    assert m.result().numpy() == np.cast['float32'](0.75)
+
+
 if __name__ == "__main__":
+    test_null_label()
+    test_with_tf()
     test_metric()