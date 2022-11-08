521436f875f3257042d60dc7f620db5dfd6f8003
NEGU93 <joseagustin.barra@gmail.com>
2022-11-08 10:26:29 +0100

    removed deprecated dependency

--- setup.py
+++ setup.py
@@ -2,7 +2,8 @@ from setuptools import setup
 import versioneer
 
 requirements = [
-    'tensorflow>=2.0', 'tensorflow-probability', 'tensorflow-addons',    # tfp for the Batch Norm (covariance)
+    'tensorflow>=2.0', 'tensorflow-probability',    # tfp for the Batch Norm (covariance)
+    # 'tensorflow-addons',
     'numpy', 'six', 'packaging',
     'pandas', 'scipy',                   # Data
     'colorlog', 'openpyxl',              # Logging
